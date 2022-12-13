dacfd1e77b4ba1820f00dfe14e6facdcb48e5b89
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2022-12-14 05:21:53 +0530

    Tailwind added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import  "./index.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App(){
+  return(
+    <>
+      <div className="text-lg font-mono text-lime-500 bg-slate-800">Hello world !</div>
+    
+    
+    </>
+  )
 }
-
-export default App;
